6ff7d06c82066ab301ac2f2291e3cfb7a8c41b0b
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2024-01-07 01:00:19 +0100

    reorder to set letter

--- src/pages/WorkoutsPage/pages/components/WorkoutExercisesTab/components/WBSet/components/WBSetHeader/components/WBReorderMenu/WBReorderMenu.tsx
+++ src/pages/WorkoutsPage/pages/components/WorkoutExercisesTab/components/WBSet/components/WBSetHeader/components/WBReorderMenu/WBReorderMenu.tsx
@@ -4,7 +4,7 @@ import {
   faCheck,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { RefObject, forwardRef } from "react";
+import { RefObject, forwardRef, useState } from "react";
 import { Form, Popover } from "react-bootstrap";
 import { ExerciseSet } from "../../../../../../../../../../../types/workout";
 
@@ -20,7 +20,10 @@ type WBReorderMenuProps = {
 const WBReorderMenu = forwardRef(
   ({ set, onReorder, ...props }: WBReorderMenuProps, ref) => {
     const { workout, setWorkout } = useWorkoutContext();
-    const setLetter = String.fromCharCode(65 + set.sort);
+
+    const [setLetter, setSetLetter] = useState<string>(
+      String.fromCharCode(65 + set.sort)
+    );
 
     const moveUp = () => {
       reorderSet(workout!.id, { setId: set.id, moveTo: set.sort - 1 }).then(
@@ -44,6 +47,18 @@ const WBReorderMenu = forwardRef(
       );
     };
 
+    const setSort = () => {
+      reorderSet(workout!.id, {
+        setId: set.id,
+        moveTo: setLetter.charCodeAt(0) - 65,
+      }).then((res) => {
+        if (res) {
+          onReorder();
+          setWorkout(res);
+        }
+      });
+    };
+
     return (
       <Popover
         ref={ref as RefObject<HTMLDivElement>}
@@ -64,13 +79,18 @@ const WBReorderMenu = forwardRef(
             </div>
             <div className="d-flex my-2">
               <Form.Control
+                pattern="[A-Z]"
+                maxLength={1}
                 className="text-end m-0"
                 value={setLetter}
-                onChange={() => {}}
+                onChange={(e) => {
+                  setSetLetter(e.target.value.toUpperCase());
+                }}
               />
               <button
                 type="button"
                 className="btn-clean btn-circle text-primary"
+                onClick={setSort}
               >
                 <FontAwesomeIcon icon={faCheck} />
               </button>
